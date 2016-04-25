95d529e3796dd9cad0c0aecfd8bf04228300337e
Mia <miarock@gmail.com>
2016-04-25 12:41:33 -0700

    fixing angular errors not clearing in create/edit

--- client/js/factory.js
+++ client/js/factory.js
@@ -124,7 +124,6 @@ myApp.factory('myAppFactory', function($http){
 //add card
 	factory.addCard = function(info, username, callback){
 		info.username = username;
-		angularErrors = [];
 		errors = [];
 		$http.post('/newCard', info).success(function(output){
 			if(output.errors){
@@ -153,7 +152,6 @@ myApp.factory('myAppFactory', function($http){
 
 //update card
 	factory.updateCard = function(card, callback){
-		angularErrors = [];
 		errors = [];
 		$http.post('/updateCard', card).success(function(output){
 			if(output.errors){
