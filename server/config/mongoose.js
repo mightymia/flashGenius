ef499b8ac98c2e31ac409f584b3350dc86e2f338
Mia <miarock@gmail.com>
2016-04-24 17:43:56 -0700

    connect to mongodb

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -2,7 +2,22 @@ var mongoose = require('mongoose');
 var fs = require('fs');
 var path = require('path');
 
-mongoose.connect('mongodb://localhost/flashGenius');
+var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }, 
+                replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };       
+ 
+var mongodbUri = 'mongodb://mclapham10:Shanny14$@ds019491.mlab.com:19491/heroku_nvx8kh20';
+ 
+mongoose.connect(mongodbUri, options);
+var conn = mongoose.connection;             
+ 
+conn.on('error', console.error.bind(console, 'connection error:'));  
+ 
+conn.once('open', function() {
+  // Wait for the database connection to establish, then start the app.                         
+});
+
+
+// mongoose.connect('mongodb://localhost/flashGenius');
 
 var models_path = __dirname + '/../models';
 
